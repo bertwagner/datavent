1295c1d0eaa491d6bfe8377632a968755f1ac550
Bert Wagner <bertwagner@berwagner.com>
2023-06-19 06:57:39 -0400

    moves pages to fastapi

--- app/main.py
+++ app/main.py
@@ -9,10 +9,19 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+@app.get("/register", response_class=HTMLResponse)
+async def schedule(request: Request):
+    return templates.TemplateResponse("register.html",{"request":request})
 
 @app.get("/schedule", response_class=HTMLResponse)
 async def schedule(request: Request):
     return templates.TemplateResponse("schedule.html",{"request":request})
+
+@app.get("/code-of-conduct", response_class=HTMLResponse)
+async def schedule(request: Request):
+    return templates.TemplateResponse("code-of-conduct.html",{"request":request})
+
