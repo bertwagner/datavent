22abc62948f1c7dddd0adf7b0211fd8de2bf6dad
Bert Wagner <bertwagner@bertwagner.com>
2023-06-23 21:39:23 -0400

    add postgres connection

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -14,4 +14,10 @@
 # python manage.py flush --no-input
 # python manage.py migrate
 
+export SQL_DATABASE=$SQL_DATABASE
+export SQL_HOST=$SQL_HOST
+export SQL_USER=$SQL_USER
+export SQL_PASSWORD=$SQL_PASSWORD
+export SQL_PORT=$SQL_PORT
+
 exec "$@"
--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+import psycopg2
+
 app = FastAPI()
+sql_host = os.environ.get("SQL_HOST")
+sql_port = os.environ.get("SQL_PORT")
+sql_user = os.environ.get("SQL_USER")
+sql_password = os.environ.get("SQL_PASSWORD")
+sql_database = os.environ.get("SQL_DATABASE")
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -27,3 +37,13 @@ async def schedule(request: Request):
 @app.get("/code-of-conduct", response_class=HTMLResponse)
 async def code_of_conduct(request: Request):
     return templates.TemplateResponse("code-of-conduct.html", {"request": request})
+
+@app.get("/pg")
+async def pg():
+    conn = psycopg2.connect(f"dbname='{sql_database}' host='{sql_host}' port='{sql_port}' user='{sql_user}' password='{sql_password}'")
+    with conn as c:
+        cur = c.cursor()
+        cur.execute("SELECT 'a' as test;")
+        records = cur.fetchall()
+
+    return {'message': "connected!"}
