9e4a1652a751e628cf180708df88c005b12e3b8a
Bert Wagner <bertwagner@bertwagner.com>
2023-06-23 22:31:14 -0400

    init product queries

---
+++ database/insert_product.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name,description,price) VALUES ('Registration','All-day conference admission.', 0);
+INSERT INTO product (name,description,price) VALUES ('Lunch','Includes chicken wrap OR veggie wrap, veggie chili, meat toppings, and more.', 16);
+INSERT INTO product (name,description,price) VALUES ('Power BI Tutorial','Registration for the Power BI half-day Tutorial.', 90);
